3d287252dce04e30ec454f1584ce8ce2926f602a
torianyap <torianyap@gmail.com>
2020-11-20 09:51:53 +0700

    revisi error banner

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,12 +1,9 @@
 <template>
   <div id="MyCarousel" class="carousel slide" data-ride="carousel">
       <div class="carousel-inner">
-          <div class="carousel-item active">
-              <img class="d-block w-100" :src="this.banner.image_url" alt="First slide" height="500px">
-          </div>
-          <div v-for="banner in banners" :key="banner.id" class="carousel-item">
-              <img class="d-block w-100" :src="banner.image_url" alt="Banner" height="500px">
-          </div>
+        <div v-for="(banner, i) in banners" :key="banner.id" class="carousel-item" :class="i === 0 ? 'active' : ''">
+          <img :src="banner.image_url" alt="slide" height="500px" class="d-block w-100">
+        </div>
       </div>
       <a class="carousel-control-prev" href="#MyCarousel" role="button" data-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -23,11 +20,8 @@
 export default {
   name: 'Carousel',
   computed: {
-    banner () {
-      return this.$store.state.banners[0]
-    },
     banners () {
-      return this.$store.state.banners.slice(1)
+      return this.$store.state.banners
     }
   },
   created () {
